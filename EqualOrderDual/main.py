3aff209565a0650b971a3b54c081eaa877f1039c
Julian Roth <julian.roth1998@gmx.de>
2023-09-09 16:41:57 +0200

    Created class for TemporalMesh with plotting and refinement routine

--- EqualOrderDual/main.py
+++ EqualOrderDual/main.py
@@ -1,2 +1,76 @@
-print("TODO")
+####################################################################
+# Temporal adaptivity for the heat equation                        #
+# with backward Euler in time (dG(0)) for primal and dual problem  #
+#                                                                  #
+# Author: Julian Roth                                              #
+# Year:   2023                                                     #
+####################################################################
+from fenics import *
+import numpy as np
+import matplotlib.pyplot as plt
 
+class TemporalMesh:
+    def __init__(self, t0, T, Δt):
+        self.t0 = t0
+        self.T = T
+        self.Δt = Δt
+        self.n_elements = int((T - t0) / Δt)
+        self.create_mesh()
+
+    def create_mesh(self):
+        self.mesh = [np.array([self.t0, self.t0 + self.Δt])]
+        for i in range(1, self.n_elements):
+            self.mesh.append(self.mesh[i - 1] + self.Δt)
+    
+    def plot_mesh(self, title="Temporal Mesh", savepath=None):
+        plt.clf()
+        plt.title(title)
+        plt.plot([self.mesh[0][0], self.mesh[-1][1]], [0., 0.], color="black")
+        plt.plot([self.t0, self.t0], [-0.1, 0.1], color="black")
+        for i in range(len(self.mesh)):
+            plt.plot([self.mesh[i][1], self.mesh[i][1]], [-0.1, 0.1], color="black")
+        # remove x and y ticks
+        plt.xticks([])
+        plt.yticks([])
+        # set x and y limits
+        plt.xlim([self.t0-self.Δt, self.T+self.Δt])
+        plt.ylim([-1., 1.])
+        # remove all spines
+        for spine in plt.gca().spines.values():
+            spine.set_visible(False)
+        if savepath is None:
+            plt.show()
+        else:
+            plt.savefig(savepath, bbox_inches='tight')
+
+    def refine(self, refine_flags=None):
+        if refine_flags is None:
+            refine_flags = [True for i in range(self.n_elements)]
+
+        new_mesh = []
+        for flag, element in zip(refine_flags, self.mesh):
+            if flag:
+                midpoint = (element[0] + element[1]) / 2.
+                new_mesh.append(np.array([element[0], midpoint]))
+                new_mesh.append(np.array([midpoint, element[1]]))
+            else:
+                new_mesh.append(element)
+        self.mesh = new_mesh
+        self.n_elements = len(self.mesh)
+
+        
+if __name__ == "__main__":
+    # hyperparameters
+    ERROR_TOL = 1e-4 # stopping criterion for DWR loop
+    temporal_mesh = TemporalMesh(
+        t0 = 0.0, # start time 
+        T = 5.0, # end time
+        Δt = 0.25 # initial uniform time step size
+    )
+
+    for i in range(5):
+        temporal_mesh.refine(refine_flags=[False]*(temporal_mesh.n_elements-1) + [True])
+    temporal_mesh.plot_mesh()
+    quit()
+
+    
